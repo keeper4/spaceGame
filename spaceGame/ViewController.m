6aed2ddf9bcd8c50e0456d7792bb6d06cf875643
Oleksandr Chyzh <a.v.chyzh@gmail.com>
2016-03-17 22:45:26 +0200

    add: animation spaceShip, image spaceShip.

--- spaceGame/ViewController.m
+++ spaceGame/ViewController.m
@@ -10,9 +10,12 @@
 
 @interface ViewController ()
 
-@property(weak,nonatomic) UIView *leftBlock;
-@property(weak,nonatomic) UIView *rightBlock;
-@property(weak,nonatomic) UIView *spaceShipBlock;
+@property (weak,nonatomic) UIView *leftBlock;
+@property (weak,nonatomic) UIView *rightBlock;
+@property (weak,nonatomic) UIView *spaceShipBlock;
+@property (nonatomic,strong) UILongPressGestureRecognizer *lpgr;
+@property (assign,nonatomic) CGFloat widthShip;
+@property (assign,nonatomic) CGFloat heigthShip;
 
 @end
 
@@ -20,19 +23,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    UIView *spaceShip = [[UIView alloc]initWithFrame:
-                         CGRectMake(CGRectGetMidX(self.view.bounds)-10,
-                                    CGRectGetMaxY(self.view.bounds)-20,
-                                                                    20,
-                                                                    20)];
     
-    spaceShip.backgroundColor = [UIColor grayColor];
+//spaceShip Create
+    
+    self.widthShip = 80;
+    self.heigthShip = 80;
+    
+    UIImageView *spaceShip = [[UIImageView alloc]initWithFrame:
+                         CGRectMake(CGRectGetMidX(self.view.bounds)-self.widthShip/2,
+                                    CGRectGetMaxY(self.view.bounds)-self.heigthShip,
+                                                                    self.widthShip,
+                                                                    self.heigthShip)];
+    
+    spaceShip.backgroundColor = [UIColor clearColor];
     
     self.spaceShipBlock = spaceShip;
     self.spaceShipBlock.layer.zPosition = 1;
     [self.view addSubview:spaceShip];
+    
+    UIImage *imageSpaceShip = [UIImage imageNamed:@"spaceShip.png"];
+    spaceShip.image = imageSpaceShip;
+    
 
+//left Tap View
     
     UIView *leftView = [[UIView alloc]initWithFrame:
                         CGRectMake(0, 0, CGRectGetMidX(self.view.bounds)-10, CGRectGetHeight(self.view.bounds))];
@@ -42,22 +55,37 @@
     [self.view addSubview:leftView];
     self.leftBlock = leftView;
     
+ //right Tap View
+    
     UIView *rightView = [[UIView alloc]initWithFrame:
                          CGRectMake(CGRectGetMidX(self.view.bounds)+10, 0, CGRectGetMidX(self.view.bounds), CGRectGetHeight(self.view.bounds))];
-    
+
     rightView.backgroundColor = [UIColor clearColor];
     rightView.tag = 2;
     [self.view addSubview:rightView];
     self.rightBlock = rightView;
     
+/// longTap
     
-
+  /*  self.lpgr = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPressGestures:)];
+    self.lpgr.minimumPressDuration = 1.0f;
+    self.lpgr.allowableMovement = 100.0f;
+    
+    [self.view addGestureRecognizer:self.lpgr];
+*/
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+#pragma mark - Method
+/*
+- (void)handleLongPressGestures:(UILongPressGestureRecognizer *)sender
+{
+    if ([sender isEqual:self.lpgr]) {
+        if (sender.state == UIGestureRecognizerStateBegan)
+        {
+            NSLog(@"handleLongPressGestures");
+        }
+    }
+} */
 
 #pragma mark - Touch
 
@@ -74,18 +102,33 @@
     if ([self.leftBlock pointInside:pointLeft withEvent:event]) {
         
         CGFloat newX = oldX-delta;
-        self.spaceShipBlock.center = CGPointMake(newX, CGRectGetMaxY(self.view.frame)-10);
-        NSLog(@"left block - %f",newX);
+        [UIView animateWithDuration:0.3f
+                              delay:0
+                            options:UIViewAnimationOptionCurveLinear
+                         animations:^{
+                             self.spaceShipBlock.center = CGPointMake(newX, CGRectGetMaxY(self.view.frame)-self.widthShip/2);
+                         }
+                         completion:^(BOOL finished) {
+                         }];
+        
     }
     if ([self.rightBlock pointInside:pointRight withEvent:event]) {
         
 
         CGFloat newX = oldX+delta;
-        self.spaceShipBlock.center = CGPointMake(newX, CGRectGetMaxY(self.view.frame)-10);
-        NSLog(@"right block - %f",newX);
+        
+        [UIView animateWithDuration:0.3f
+                              delay:0
+                            options:UIViewAnimationOptionCurveLinear
+                         animations:^{
+                             self.spaceShipBlock.center = CGPointMake(newX, CGRectGetMaxY(self.view.frame)-self.widthShip/2);
+                         }
+                         completion:^(BOOL finished) {
+                         }];
+
+
     }
-   
-    
+ 
     
 }
 
