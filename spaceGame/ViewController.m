91c3983ba4bf10869d6cb992bf6f99aec308673e
Yaroslav Chyzh <tommyizua@gmail.com>
2016-03-19 20:37:37 +0200

    Provided animation background with image.

--- spaceGame/ViewController.m
+++ spaceGame/ViewController.m
@@ -20,13 +20,66 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-//spaceShip Create
+    //spaceShip Create
     
     ACSpaceShip *spaceShip = [[ACSpaceShip alloc]init];
-
+    
     [self.view addSubview:spaceShip];
     
     self.spaceShipBlock = spaceShip;
+    
+    
+    [self setupBackgroundImageViews];
+    
+}
+
+#pragma mark - Setup Animation Background
+
+- (void)setupBackgroundImageViews {
+    
+    UIImage *backgroundImage = [UIImage imageNamed:@"background"];
+    UIImageView *backgroundViewFirst = [[UIImageView alloc] initWithImage:backgroundImage];
+    
+    CGRect viewRect = self.view.frame;
+    
+    backgroundViewFirst.frame = CGRectMake(CGRectGetMinX(viewRect),
+                                           CGRectGetMinX(viewRect),
+                                           CGRectGetWidth(viewRect),
+                                           CGRectGetHeight(viewRect));
+    
+    UIImageView *backgroundViewSecond = [[UIImageView alloc] initWithImage:backgroundImage];
+    
+    backgroundViewSecond.frame = CGRectMake(CGRectGetMinX(viewRect),
+                                            -CGRectGetHeight(viewRect),
+                                            CGRectGetWidth(viewRect),
+                                            CGRectGetHeight(viewRect));
+    
+    
+    [self.view addSubview:backgroundViewFirst];
+    
+    [self.view addSubview:backgroundViewSecond];
+    
+    
+    [self moveBackgroundWithFirstView:backgroundViewFirst secondView:backgroundViewSecond andDuration:10];
+    
+}
+
+- (void)moveBackgroundWithFirstView:(UIImageView *)firstView secondView:(UIImageView *)secondView andDuration:(NSTimeInterval)duration {
+    
+    [UIView animateWithDuration:duration
+                          delay:0
+                        options:UIViewAnimationOptionRepeat | UIViewAnimationOptionCurveLinear
+                     animations:^{
+                         
+                         firstView.frame = CGRectMake(CGRectGetMinX(firstView.frame),
+                                                                CGRectGetMaxY(firstView.frame),
+                                                                CGRectGetWidth(firstView.frame),
+                                                                CGRectGetHeight(firstView.frame));
+                         
+                         secondView.frame = firstView.frame;
+                         
+                     } completion:nil];
+    
 }
 
 #pragma mark - Touch
@@ -39,7 +92,7 @@
     UITouch *touch = [touches anyObject];
     
     CGPoint pointTouch = [touch locationInView:self.view];
-
+    
     if (pointTouch.x <= CGRectGetMidX(self.view.frame) &&
         CGRectGetMinX(self.view.frame) <= CGRectGetMinX(self.spaceShipBlock.frame)-delta) {
         
